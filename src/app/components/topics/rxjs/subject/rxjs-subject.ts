631ec9adac4bd8180fc75df8449d6107144aa497
SoumyaSRD <soumyarjnd@gmail.com>
2026-02-13 11:27:40 +0530

    fixed issue

--- src/app/components/topics/rxjs/subject/rxjs-subject.ts
+++ src/app/components/topics/rxjs/subject/rxjs-subject.ts
@@ -12,4 +12,45 @@ import { ITopicContent } from "../../../../interfaces/topic";
 })
 export class RxjsSubject {
     content: ITopicContent | any = RXJS_SUBJECT;
+    subjectExample = `
+const subject = new Subject<number>();
+
+subject.subscribe(value => console.log('Subscriber A:', value));
+subject.subscribe(value => console.log('Subscriber B:', value));
+
+subject.next(1);
+subject.next(2);
+`;
+
+    behaviorSubjectExample = `
+const behavior = new BehaviorSubject<number>(0);
+
+behavior.subscribe(value => console.log('First:', value));
+
+behavior.next(1);
+behavior.next(2);
+
+behavior.subscribe(value => console.log('Second:', value));
+`;
+
+    replaySubjectExample = `
+const replay = new ReplaySubject<number>(2);
+
+replay.next(1);
+replay.next(2);
+replay.next(3);
+
+replay.subscribe(value => console.log(value));
+`;
+
+    asyncSubjectExample = `
+const async = new AsyncSubject<number>();
+
+async.subscribe(value => console.log(value));
+
+async.next(1);
+async.next(2);
+async.complete();
+`;
+
 }
